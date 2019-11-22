3fd42da966c78f57eed40d9b9c453ed90a0087df
BuzatuCos <allkindcreation@gmail.com>
2019-11-22 13:08:25 +0000

    Remove console.log

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let portItems = document.querySelectorAll('.portBox')
 portItems.forEach((port)=>{
     port.addEventListener('mouseover', (e) => {
-        console.log(e.target.childNodes[1])
         e.target.childNodes[1].classList.remove('hidden')
     })
     port.addEventListener('mouseleave', (e) => {
